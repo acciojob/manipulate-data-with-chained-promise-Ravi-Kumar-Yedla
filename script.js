336797b61db0d4e5137d4fd247b9bd59bb9a3073
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-21 10:10:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function manipulateArray(arr) {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(arr);
+		},3000)
+	});
+	.then(arr=>{
+		const oddNumbers = arr.filter(num => num%2 !==0);
+		document.getElementById('output').innerHTML = `odd: ${oddNumbers}`;
+		return arr.filter(num => num%2 ===0);
+	})
+	.then(arr=>{
+		const evenNumbers = arr.map(num => num*2);
+		document.getElementById('output').innerHTML = `multiplied for every even numbers: ${evenNumbers}`;
+		
+	});
+}
